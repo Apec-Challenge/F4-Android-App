e3b67a99eb5069d415602c3cd997c5ea047491ad
H43RO <h2is1234@naver.com>
2020-10-24 22:25:19 +0900

    기능:: Google Map View 추가
    메인 화면에서 사용자의 주변 장소 (호텔, 펜션 등) 의 정보를
    알아볼 수 있도록 Google Map View 를 구현했음 (마커 제공 예정)

--- app/src/main/java/com/k_rona/funding4/navigation/home/HomeFragment.kt
+++ app/src/main/java/com/k_rona/funding4/navigation/home/HomeFragment.kt
@@ -1,18 +1,27 @@
 package com.k_rona.funding4.navigation.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.k_rona.funding4.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var map: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,5 +34,31 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment: SupportMapFragment? =
+            (activity?.supportFragmentManager?.findFragmentById(R.id.home_near_by_map)) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        // TODO: 2020/10/24 onMapReady() Callback Method 진입이 안되는 현상 발생 
+        map = p0!!
+
+        var SEOUL = LatLng(37.56, 126.97)
+
+        var markerOptions: MarkerOptions = MarkerOptions()
+        markerOptions
+            .position(SEOUL)
+            .title("서울")
+            .snippet("한국 수도")
+
+        map.addMarker(markerOptions)
+        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL))
+
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10F))
     }
 }
